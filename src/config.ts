0353c8466d3bf4ab254ac61edb3db51f0b75738f
zxykevin <2596864417@qq.com>
2026-05-14 16:32:51 +0200

    Replace default Mizuki favicon

--- src/config.ts
+++ src/config.ts
@@ -56,7 +56,7 @@ export const siteConfig: SiteConfig = {
 		mode: "text-icon",
 		// 顶栏标题文本
 		text: "星轨手札",
-		// 顶栏标题图标路径，默认使用 public/assets/home/home.webp
+		// 顶栏标题图标路径
 		icon: "assets/home/home.webp",
 		// 网站Logo图片路径
 		logo: "assets/home/default-logo.webp",
@@ -203,7 +203,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		// 留空以使用默认 favicon
 		// {
-		//   src: '/favicon/icon.png',    // 图标文件路径
+		//   src: '/favicon.png',         // 图标文件路径
 		//   theme: 'light',              // 可选，指定主题 'light' | 'dark'
 		//   sizes: '32x32',              // 可选，图标大小
 		// }
